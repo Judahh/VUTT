2e437abf545e0346f62af0590a53c5649a62fc18
Judah <judahholanda7@gmail.com>
2021-11-28 15:53:00 -0300

    improve handler

--- src/pages/api/v1/tools/toolHandler.ts
+++ src/pages/api/v1/tools/toolHandler.ts
@@ -1,7 +1,6 @@
 import { request, RouterSingleton } from '@backapirest/next';
 import dbHandler from '../../../../dBHandler';
 import Index from '../../../../routes';
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default (req: Request, res: Response) => {
   return request(
     req,
